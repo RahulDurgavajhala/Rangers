1d7996d76da5e98bffc4497b68f06a970b1cc161
Dib <126925086+RahulDurgavajhala@users.noreply.github.com>
2023-07-10 17:55:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ function new_image(get_image)
 		block_image_object.scaleToWidth(block_image_width)
 		block_image_object.scaleToHeight(block_image_height)
 		block_image_object.set({
-			top: block_y
+			top: block_y ,
+			
 			left: hole_x
 		})
 		canvas.add(block_image_object)
